cd6460b619544f5a3dea711d7b91e89fbc7c83b2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-29 08:43:40 +0100

    Added rateLimiter

---
+++ middlewares/rateLimiter.js
@@ -0,0 +1,14 @@
+import { rateLimit } from "express-rate-limit";
+
+const rateLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: config.get("App.request-limit"),
+  message: {
+    code: 429,
+    status: "Too many requests",
+    message: "Too many requests chief, try again later",
+    data: {},
+  },
+});
+
+export default rateLimiter;
