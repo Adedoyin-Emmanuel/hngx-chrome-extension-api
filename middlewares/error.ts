c8780af71f5a129e59f97fba2e19ee521cd8a893
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-02 05:20:08 +0100

    Bug fix

--- middlewares/error.ts
+++ middlewares/error.ts
@@ -3,7 +3,7 @@ import response from "../utils/response";
 
 const errorHandler = (err: Error, req: Request, res: Response, next: any) => {
   console.log(err);
-  return response(res, 500, "Something went wrong, please try again later!");
+  return response(res, 500, `Something went wrong, please try again later!n ${err}`);
 };
 
 export default errorHandler;
