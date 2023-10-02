dd526627ed95f4ebcb8fe15f820568916abc2408
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-02 05:21:06 +0100

    Updates

--- dist/controllers/video.controller.js
+++ dist/controllers/video.controller.js
@@ -19,65 +19,6 @@ const path_1 = __importDefault(require("path"));
 const models_1 = __importDefault(require("../models"));
 const utils_1 = require("../utils");
 class VideoController {
-    // static async uploadVideo(req: Request, res: Response) {
-    //   const requestSchema = Joi.object({
-    //     title: Joi.string().required().max(100),
-    //   });
-    //   const maxVideoSize: number = config?.get("App.max-video-size");
-    //   let uploadedFileName = "";
-    //   const storage = multer.diskStorage({
-    //     destination: (req, file, cb) => {
-    //       cb(null, "./uploads/");
-    //     },
-    //     filename: (req, file, cb) => {
-    //       uploadedFileName = uuidv4() + "-" + file.originalname;
-    //       cb(null, uploadedFileName);
-    //     },
-    //   });
-    //   const upload = multer({
-    //     storage: storage,
-    //     limits: { fileSize: maxVideoSize },
-    //   });
-    //   upload.single("video")(req, res, async (err) => {
-    //     const { error, value } = requestSchema.validate(req.body);
-    //     const { title } = value;
-    //     if (error) return response(res, 400, error.details[0].message);
-    //     if (err) {
-    //       console.log(err);
-    //       return response(res, 400, `File upload failed, ${err.message}`);
-    //     }
-    //     if (!req.file) return response(res, 400, "File is required");
-    //     const fileName = path.join(
-    //       __dirname,
-    //       "..",
-    //       `/uploads/${uploadedFileName}`
-    //     );
-    //     const readFile = fs.createReadStream(fileName);
-    //     const OpenAi = new OpenAI({
-    //       apiKey: process.env.OPEN_AI_API_KEY,
-    //     });
-    //     const transcriptionParams = {
-    //       file: readFile,
-    //       model: "whisper-1",
-    //     };
-    //     try {
-    //       const aiTranscript = await OpenAi.audio.transcriptions.create(
-    //         transcriptionParams
-    //       );
-    //       const { text: transcript } = aiTranscript;
-    //       const dataToStore = {
-    //         title,
-    //         transcript,
-    //         url: `${req.get("host")}/assets/${uploadedFileName}`,
-    //       };
-    //       // save the data in the db
-    //       const dbResponse = await VideoModel.create(dataToStore);
-    //       return response(res, 200, "Video upload successful", dbResponse);
-    //     } catch (error) {
-    //       console.error("Error:", error);
-    //     }
-    //   });
-    // }
     static startStream(req, res) {
         var _a, _b;
         return __awaiter(this, void 0, void 0, function* () {
--- dist/middlewares/error.js
+++ dist/middlewares/error.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const response_1 = __importDefault(require("../utils/response"));
 const errorHandler = (err, req, res, next) => {
     console.log(err);
-    return (0, response_1.default)(res, 500, "Something went wrong, please try again later!");
+    return (0, response_1.default)(res, 500, `Something went wrong, please try again later!n ${err}`);
 };
 exports.default = errorHandler;
